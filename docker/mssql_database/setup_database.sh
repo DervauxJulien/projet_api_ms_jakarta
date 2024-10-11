e2d8f7afed3d2990c0a4cde54d773796bb81ca3b
BenjaminT <btermeau@gmail.com>
2024-10-11 08:15:00 +0200

    modify setup_database.sh to add pass

--- docker/mssql_database/setup_database.sh
+++ docker/mssql_database/setup_database.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Wait for database to startup 
 sleep 20
-./opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P <YOUR PASSWORD> -i setup.sql
+./opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P azerty@123456 -i setup.sql
