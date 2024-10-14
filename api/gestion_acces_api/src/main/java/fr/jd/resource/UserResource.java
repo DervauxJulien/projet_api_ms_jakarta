c9359645c5a35f66df9da224886fd33a274bd029
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-10-14 15:56:17 +0200

    Création de getAllUsers, getUserById, deleteOneUser dans UserResource

--- api/gestion_acces_api/src/main/java/fr/jd/resource/UserResource.java
+++ api/gestion_acces_api/src/main/java/fr/jd/resource/UserResource.java
@@ -12,8 +12,9 @@ import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.List;
 
-@Path("/user")
+@Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UserResource {
@@ -40,6 +41,7 @@ public class UserResource {
 
         Adress adress = adressResource.createAdress(createAdressData);
 
+
         createUserData.username = userData.username;
         createUserData.password = userData.password;
         createUserData.email = userData.email;
@@ -53,4 +55,35 @@ public class UserResource {
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
+
+    @GET
+    public List<Users> getAllUsers() {
+        return Users.listAll();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Users getUserById(@PathParam("id") int id) {
+        Users entity = Users.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        return entity;
+    }
+
+    @DELETE
+    @Transactional
+    @Path("delete/{id}")
+    public Response deleteOne(@PathParam("id") Long id) {
+        Users entity = Users.findById(id);
+        if (entity == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        entity.delete();
+        return Response.noContent().build();
+    }
+
+
+
 }
