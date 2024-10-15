0c6e8a867e9e2a8b3cf1c3630a132dab1f32f3af
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-10-15 16:27:26 +0200

    ajout du PATCH pour user

--- api/gestion_acces_api/src/test/java/fr/jd/ExampleResourceIT.java
+++ api/gestion_acces_api/src/test/java/fr/jd/ExampleResourceIT.java
@@ -3,6 +3,6 @@ package fr.jd;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-class ExampleResourceIT extends ExampleResourceTest {
+class ExampleResourceIT extends UserResourceTest {
     // Execute the same tests but in packaged mode.
 }
