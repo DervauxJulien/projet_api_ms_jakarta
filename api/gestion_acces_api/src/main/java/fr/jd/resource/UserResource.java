9d7d2a5ec31bd496558df5f516ecf6337f7f3cae
Delhumeur <126495668+DervauxJulien@users.noreply.github.com>
2024-10-15 08:48:18 +0200

    Ajout du patch pour la modification du compte users

--- api/gestion_acces_api/src/main/java/fr/jd/resource/UserResource.java
+++ api/gestion_acces_api/src/main/java/fr/jd/resource/UserResource.java
@@ -1,5 +1,6 @@
 package fr.jd.resource;
 
+import fr.jd.entity.Adress;
 import fr.jd.entity.Users;
 import fr.jd.repository.UserRepository;
 import jakarta.inject.Inject;
@@ -32,26 +33,58 @@ public class UserResource {
         return entity;
     }
 
-    @PATCH
     @Transactional
+    @PATCH
     @Path("update/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response updateUser(@PathParam("id") Long id, Users userUpdates) {
+    public Response updateUser(@PathParam("id") Integer id, Users userUpdates) {
         Users entity = Users.findById(id);
         if (entity == null) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
+
         if (userUpdates.getUsername() != null) {
             entity.setUsername(userUpdates.getUsername());
         }
         if (userUpdates.getEmail() != null) {
             entity.setEmail(userUpdates.getEmail());
         }
-        entity.persist();
+        if (userUpdates.getPassword() != null) {
+            entity.setPassword(userUpdates.getPassword());
+        }
+        if (userUpdates.getRole() != null) {
+            entity.setRole(userUpdates.getRole());
+        }
+        if (userUpdates.isLock() != entity.isLock()) {
+            entity.setLock(userUpdates.isLock());
+        }
+        if (userUpdates.getLast_connection() != null) {
+            entity.setLast_connection(userUpdates.getLast_connection());
+        }
+        if (userUpdates.getPin() != null) {
+            entity.setPin(userUpdates.getPin());
+        }
+
+        if (userUpdates.getAdress() != null) {
+            if (entity.getAdress() == null) {
+                entity.setAdress(userUpdates.getAdress());
+            } else {
+                Adress updatedAdress = userUpdates.getAdress();
+                Adress currentAdress = entity.getAdress();
+
+                if (updatedAdress.getStreet() != null) {
+                    currentAdress.setStreet(updatedAdress.getStreet());
+                }
+                if (updatedAdress.getCity() != null) {
+                    currentAdress.setCity(updatedAdress.getCity());
+                }
+                if (updatedAdress.getZip() != null) {
+                    currentAdress.setZip(updatedAdress.getZip());
+                }
+            }
+        }
         return Response.ok(entity).build();
     }
-
-
 
     @DELETE
     @Transactional
